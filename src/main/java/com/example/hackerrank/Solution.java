8b8d48279c415f033f68c7d7a84646f3e7a4411a
Ryggs <joshryggs@gmail.com>
2023-02-21 08:13:19 +0300

    Scanner and else-if Test

---
+++ src/main/java/com/example/hackerrank/Solution.java
@@ -0,0 +1,22 @@
+package com.example.hackerrank;
+
+import java.io.InputStream;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        String myString = scan.next();
+
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(myString);
+        System.out.println(scan.next());
+    }
+
+
+}
