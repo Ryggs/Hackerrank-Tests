b3f71b449493cb35fdedff5cf0493b60b37ad2c2
Ryggs <joshryggs@gmail.com>
2023-02-23 10:24:16 +0300

    mainJava

---
+++ src/main/java/com/example/hackerrank/answer.java
@@ -0,0 +1,32 @@
+package com.example.hackerrank;
+
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
+
+
+class Result {
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n) {
+
+    }
+
+}
+
+public class answer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.fizzBuzz(n);
+
+        bufferedReader.close();
+    }
+}
